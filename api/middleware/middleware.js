b446438322be74917201c224e127eb19411c3889
Jeremiah Candelaria <jeremiahcandelaria@gmail.com>
2022-02-02 13:42:20 -0800

    complete mvp tasks, all tests pass

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,26 @@
+const User = require('../users/users-model');
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(`A ${req.method} request happened on ${req.url} at ${Date.now()}`);
+  next();
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  const { id } = req.params;
+  User.getById(id)
+    .then(possibleUser => {
+      possibleUser ? (req.user = possibleUser, next()) : res.status(404).json({ message: "user not found" });
+    })
+    .catch(next)
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  req.body.name ? next() : res.status(400).json({ message: "missing required name field" });
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  req.body.text ? next() : res.status(400).json({ message: "missing required text field" });
 }
 
 // do not forget to expose these functions to other modules
+module.exports = { logger, validateUserId, validateUser, validatePost }
